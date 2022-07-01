18d61f87386fd4da8a905ac5e37de58adc12f613
Josh Hartwig <joshman211@gmail.com>
2022-07-01 14:34:17 -0500

    updated css

---
+++ index.js
@@ -0,0 +1,18 @@
+// module pattern for game
+let Game = (function (){
+    let Game = {};
+    //let _privateMethod = function(){}
+
+    //Game.PublicMethod = function(){}
+    return Game;
+})();
+
+
+// factory pattern
+  const Player = (name) => {
+    const describe = () => console.log('described');
+    return {describe}
+  }
+
+  const player1 = new Player('bob');
+  player1.describe();
